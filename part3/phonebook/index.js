1b9cd93ef27cf844d1fcec0d9b672342ebd943b5
judd147 <zhangliyao990311@outlook.com>
2023-04-19 15:31:30 +0800

    finished 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,12 +5,27 @@ const morgan = require('morgan')
 morgan.token('content', function getContent (req) {
   return JSON.stringify(req.body)
 })
+const Person = require('./models/person') // import the mongoose model
 const app = express() // create an express application
+
+// handler of requests with unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// custom Express error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' }) // respond with status code 400 Bad Request
+  } 
+  next(error) // else, passes the error forward to the default Express error handler
+}
+
 app.use(express.json()) // use express json parser to help access data
 app.use(cors()) // use cors to allow for requests from all origins
 app.use(express.static('build')) // make express show static content
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content')) // use morgan for logging in custom format
-const Person = require('./models/person') // import the mongoose model
 
 // Route for fetching all resources
 app.get('/api/persons', (request, response) => {
@@ -28,18 +43,39 @@ app.get('/info', (request, response) => {
 })
 
 // Route for fetching a single resource
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+    // error handling
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }) // additional catch if promise is rejected
+  .catch(error => next(error)) // continue to the custom error handler middleware
 })
 
 // Route for deleting a single resource; Test with REST Client
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id) // filter out deleted resource
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id).then(result => {
+    response.status(204).end() // respond with status code 204 no content
+  })
+  .catch(error => next(error)) // continue to the custom error handler middleware
+})
 
-  response.status(204).end() // respond with status code 204 no content
+// Route for updating resources(update number)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true }) // cause our event handler to be called with the new modified document
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error)) // continue to the custom error handler middleware
 })
 
 // Route for adding a new resource; Test with REST Client
@@ -56,13 +92,7 @@ app.post('/api/persons', (request, response) => {
     error: 'number missing'
     })
   }
-  /*
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({
-    error: 'name must be unique'
-    })
-  }
-  */
+
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -72,6 +102,9 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.use(unknownEndpoint) // next to the last middleware
+app.use(errorHandler) // this has to be the last loaded middleware
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
